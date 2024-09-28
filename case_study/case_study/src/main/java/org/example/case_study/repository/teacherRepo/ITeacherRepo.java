0c7ce29baa2227d9e546305dd951b8078c57fb20
Le Quang Viet <166206730+itachialy@users.noreply.github.com>
2024-09-28 14:19:40 +0700

    Delete case_study/case_study directory

--- case_study/case_study/src/main/java/org/example/case_study/repository/teacherRepo/ITeacherRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.case_study.repository.teacherRepo;
-
-import org.example.case_study.model.Teacher;
-
-import java.util.List;
-
-public interface ITeacherRepo {
-    List<Teacher> findAll();
-}
